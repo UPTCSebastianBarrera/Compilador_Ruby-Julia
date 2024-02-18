c930cc6631a5bec028cc55149eec52c1070d4340
UPTCSebastianBarrera <joan.barrera01@gmail.com>
2024-02-18 16:50:24 -0500

    analisis semantico de julia y ruby agregado

--- main.py
+++ main.py
@@ -12,22 +12,73 @@ def set_file_path(path):
     file_path = path
 
 
-def open_file():
-    
-    path = askopenfilename(filetypes=[('Julia/Ruby Files', '*.jl .rb')])
-    with open(path, 'r') as file:
-        code = file.read()
-        code_output.delete("1.0",END)
-        code_output.insert("1.0", detect_language(code))
-        editor.delete('1.0', END)
-        editor.insert('1.0', code)
-        set_file_path(path)
+class SemanticsAnalyzer:
+    def __init__(self):
+        self.symbol_table = {'global': {}}
+        self.current_scope = 'global'
+        self.errors = []  # Lista para almacenar mensajes de error semántico
+
+    def analyze_semantics(self, code, language):
+        if language == 'ruby':
+            self.analyze_ruby_semantics(code)
+        elif language == 'julia':
+            self.analyze_julia_semantics(code)
+
+    def analyze_ruby_semantics(self, code):
+        # Verificar la correcta definición y uso de variables
+        variables = re.findall(r'\b(\w+)\s*=\s*(.+?)\b', code)
+        for variable, value in variables:
+            if variable in self.symbol_table[self.current_scope]:
+                self.errors.append(f"Advertencia: Variable '{variable}' ya definida en el ámbito actual.")
+            else:
+                self.symbol_table[self.current_scope][variable] = value
+
+        # Verificar llamadas a funciones y sus argumentos
+        function_calls = re.findall(r'\b(\w+)\((.*?)\)\b', code)
+        for function, arguments in function_calls:
+            if function not in self.symbol_table[self.current_scope]:
+                self.errors.append(f"Error: Función '{function}' no definida.")
+            else:
+                # Verificar la correspondencia entre los argumentos de la función y sus parámetros
+                expected_parameters = re.findall(r'\bdef\s+%s\((.*?)\)' % function, code)
+                if expected_parameters:
+                    expected_parameters = expected_parameters[0].split(',')
+                    provided_arguments = arguments.split(',')
+                    if len(expected_parameters) != len(provided_arguments):
+                        self.errors.append(f"Error: Número incorrecto de argumentos para la función '{function}'.")
+
+    def analyze_julia_semantics(self, code):
+        # Verificar la correcta definición y uso de variables en Julia
+        variables = re.findall(r'\b(\w+)\s*=\s*(.+?)\b', code)
+        for variable, value in variables:
+            if variable in self.symbol_table[self.current_scope]:
+                self.errors.append(f"Advertencia: Variable '{variable}' ya definida en el ámbito actual.")
+            else:
+                self.symbol_table[self.current_scope][variable] = value
+
+        # Verificar llamadas a funciones y sus argumentos en Julia
+        function_calls = re.findall(r'\b(\w+)\((.*?)\)\b', code)
+        for function, arguments in function_calls:
+            if function not in self.symbol_table[self.current_scope]:
+                self.errors.append(f"Error: Función '{function}' no definida.")
+            else:
+                # Verificar la correspondencia entre los argumentos de la función y sus parámetros
+                expected_parameters = re.findall(r'\bfunction\s+%s\((.*?)\)' % function, code)
+                if expected_parameters:
+                    expected_parameters = expected_parameters[0].split(',')
+                    provided_arguments = arguments.split(',')
+                    if len(expected_parameters) != len(provided_arguments):
+                        self.errors.append(f"Error: Número incorrecto de argumentos para la función '{function}'.")
+
+    def report_errors(self):
+        return "\n".join(self.errors)
 
 
 def detect_language(content):
     patterns = {
         'julia': r'\b(julia|function|using|global|let|struct|importall|println)\b',
-        'ruby': r'\b(ruby|require|puts|def|alias|class|elsif|module)\b'    }
+        'ruby': r'\b(ruby|require|puts|def|alias|class|elsif|module)\b'
+    }
 
     detected_language = None
 
@@ -36,7 +87,28 @@ def detect_language(content):
             detected_language = language
             break
 
-    return("Lenguaje insertado es: " + detected_language)
+    return detected_language
+
+
+def open_file():
+    path = askopenfilename(filetypes=[('Julia/Ruby Files', '*.jl .rb')])
+    with open(path, 'r') as file:
+        code = file.read()
+        code_output.delete("1.0", END)
+        detected_language = detect_language(code)
+        code_output.insert("1.0", detected_language)
+        editor.delete('1.0', END)
+        editor.insert('1.0', code)
+        set_file_path(path)
+
+        semantic_analyzer = SemanticsAnalyzer()
+        semantic_analyzer.analyze_semantics(code, detected_language)
+
+        error_message = semantic_analyzer.report_errors()
+        if error_message:
+            code_output.insert(END, f"\n\nErrores Semánticos:\n{error_message}")
+        else:
+            code_output.insert(END, "\n\nNo hay errores semánticos.")
 
 
 def save_as():
@@ -48,7 +120,6 @@ def save_as():
         code = editor.get('1.0', END)
         file.write(code)
         set_file_path(path)
-
 
 
 menu_bar = Menu(compiler)
--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,12 @@
 def calcular_estadisticas(datos)
     n = datos.length
-  
+
+
     # Calcular la media
     suma = datos.reduce(:+)
     media = suma / n.to_f
   
-    # Calcular la mediana
+    # Calcular la median
     datos_ordenados = datos.sort
     if n % 2 == 0
       indice_medio1 = n / 2
@@ -16,9 +17,10 @@ def calcular_estadisticas(datos)
       mediana = datos_ordenados[indice_medio - 1]
     end
   
-    # Calcular la desviación estándar
+    # Calcular la desviacion estándar
     suma_cuadrados_diff = datos.reduce(0) { |acc, dato| acc + (dato - media)**2 }
     desviacion_estandar = Math.sqrt(suma_cuadrados_diff / n)
+    
   
     # Imprimir resultados
     puts "Media: #{media}"
@@ -26,9 +28,9 @@ def calcular_estadisticas(datos)
     puts "Desviación estándar: #{desviacion_estandar}"
   end
   
-  # Datos predefinidos
-  datos = [12.3, 45.6, 78.9, 23.4, 56.7, 89.0, 34.5, 67.8, 90.1, 43.2, 76.5, 98.7, 54.3, 87.6, 21.0, 65.4, 32.1, 43.8, 76.9, 98.2]
+# Datos predefinidos
+datos = [12.3, 45.6, 78.9, 23.4, 56.7, 89.0, 34.5, 67.8, 90.1, 43.2, 76.5, 98.7, 54.3, 87.6, 21.0, 65.4, 32.1, 43.8, 76.9, 98.2]
   
-  # Calcular estadísticas
-  calcular_estadisticas(datos)
+# Calcular estadisticas
+calcular_estadisticas(datos)
   
