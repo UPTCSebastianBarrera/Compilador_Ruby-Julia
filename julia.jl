9b4f857317d33f833ff33e9d2a11451456d57434
UPTCSebastianBarrera <joan.barrera01@gmail.com>
2024-02-25 16:18:06 -0500

    se remueve comentarios de archivos rb y jl para evitar errores

--- julia.jl
+++ julia.jl
@@ -1,11 +1,9 @@
 function calcular_estadisticas(datos)
     n = length(datos)
     
-    # Calcular la media
     suma = sum(datos)
     media = suma / n
     
-    # Calcular la mediana
     datos_ordenados = sort(datos)
     if n % 2 == 0
         indice_medio1 = n ÷ 2
@@ -16,18 +14,14 @@ function calcular_estadisticas(datos)
         mediana = datos_ordenados[indice_medio]
     end
     
-    # Calcular la desviación estándar
     suma_cuadrados_diff = sum((dato - media)^2 for dato in datos)
     desviacion_estandar = sqrt(suma_cuadrados_diff / n)
     
-    # Imprimir resultados
     println("Media: ", media)
     println("Mediana: ", mediana)
     println("Desviación estándar: ", desviacion_estandar)
 end
 
-# Datos predefinidos
 datos = [12.3, 45.6, 78.9, 23.4, 56.7, 89.0, 34.5, 67.8, 90.1, 43.2, 76.5, 98.7, 54.3, 87.6, 21.0, 65.4, 32.1, 43.8, 76.9, 98.2]
 
-# Calcular estadísticas
 calcular_estadisticas(datos)
--- ruby.rb
+++ ruby.rb
@@ -2,11 +2,9 @@ def calcular_estadisticas(datos)
     n = datos.length
 
 
-    # Calcular la media
     suma = datos.reduce(:+)
     media = suma / n.to_f
   
-    # Calcular la median
     datos_ordenados = datos.sort
     if n % 2 == 0
       indice_medio1 = n / 2
@@ -17,20 +15,15 @@ def calcular_estadisticas(datos)
       mediana = datos_ordenados[indice_medio - 1]
     end
   
-    # Calcular la desviacion estándar
     suma_cuadrados_diff = datos.reduce(0) { |acc, dato| acc + (dato - media)**2 }
     desviacion_estandar = Math.sqrt(suma_cuadrados_diff / n)
     
-  
-    # Imprimir resultados
     puts "Media: #{media}"
     puts "Mediana: #{mediana}"
     puts "Desviación estándar: #{desviacion_estandar}"
   end
   
-# Datos predefinidos
 datos = [12.3, 45.6, 78.9, 23.4, 56.7, 89.0, 34.5, 67.8, 90.1, 43.2, 76.5, 98.7, 54.3, 87.6, 21.0, 65.4, 32.1, 43.8, 76.9, 98.2]
   
-# Calcular estadisticas
 calcular_estadisticas(datos)
   
